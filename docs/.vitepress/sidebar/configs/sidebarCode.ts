7cad85209675132120af08222acecebc7da010f7
少年 <1374532240@qq.com>
2023-10-14 20:48:18 +0800

    update: 修改文件 🥳

--- docs/.vitepress/sidebar/configs/sidebarCode.ts
+++ docs/.vitepress/sidebar/configs/sidebarCode.ts
@@ -3,9 +3,9 @@ const sidebarCode = [
     text: "手写代码",
     items: [
       { text: "Introduction", link: "/advance/code/introduction" },
-      { text: "part-one", link: "/advance/code/part-one" },
-      { text: "part-two", link: "/advance/code/part-two" },
-      { text: "part-three", link: "/advance/code/part-three" },
+      { text: "代码 1", link: "/advance/code/part-one" },
+      { text: "代码 2", link: "/advance/code/part-two" },
+      { text: "代码 3", link: "/advance/code/part-three" },
     ],
   },
   {
@@ -13,7 +13,7 @@ const sidebarCode = [
     items: [
       { text: "Introduction", link: "/advance/code/introduction" },
       { text: "作用域", link: "/advance/code/变量作用域" },
-      { text: "this指向", link: "/advance/code/this" },
+      { text: "this", link: "/advance/code/this" },
       { text: "EventLoop", link: "/advance/code/introduction" },
       { text: "隐式类型转换", link: "/advance/code/类型转换" },
       { text: "执行结果", link: "/advance/code/introduction" },
