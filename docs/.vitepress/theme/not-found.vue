bcf506771a2444c0b21c79a880b5363c3197bb71
少年 <1374532240@qq.com>
2023-10-21 15:09:33 +0800

    update: 修改文件 🥳

--- docs/.vitepress/theme/not-found.vue
+++ docs/.vitepress/theme/not-found.vue
@@ -9,12 +9,7 @@ const goBack = () => {
 
 <template>
   <div style="position: absolute; top: 50%; left: 50%; translate: -50% -50%">
-    <n-result
-      status="404"
-      title="404 资源不存在"
-      size="huge"
-      description="生活总归带点荒谬"
-    >
+    <n-result description="生活总归带点荒谬" size="huge" status="404" title="404 资源不存在">
       <template #footer>
         <n-button @click="goBack">回到首页</n-button>
       </template>
