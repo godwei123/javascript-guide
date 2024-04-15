48c33fbf84cda2f26be9f18769e8f71b2f53f8e4
少年 <1374532240@qq.com>
2024-04-15 23:41:07 +0800

    update: 修改文件 🥳

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -19,24 +19,28 @@ export default {
     console.log(process.env.NODE_ENV);
 
     if (process.env.NODE_ENV !== "development") {
-      const myWorker = new Worker("/javascript-guide/public/js/webWorker.js");
-      myWorker.onmessage = function (e) {
-        const data = JSON.parse(e.data);
-        if (data.version) {
-          if (judgeVersion(data.version)) {
-            console.log(
-              `Current version is ${currentVersion}, remote version is ${data.version}, New version is available`
-            );
+      let m = app.mount;
+      app.mount = function () {
+        m("#app");
+        const myWorker = new Worker("/javascript-guide/public/js/webWorker.js");
+        myWorker.onmessage = function (e) {
+          const data = JSON.parse(e.data);
+          if (data.version) {
+            if (judgeVersion(data.version)) {
+              console.log(
+                `Current version is ${currentVersion}, remote version is ${data.version}, New version is available`
+              );
+            }
           }
-        }
+        };
+        myWorker.onmessageerror = function (e) {
+          console.log("Message error received from worker");
+        };
+        myWorker.onerror = function (e) {
+          console.log("Error received from worker");
+        };
+        myWorker.postMessage("Hello from the main thread");
       };
-      myWorker.onmessageerror = function (e) {
-        console.log("Message error received from worker");
-      };
-      myWorker.onerror = function (e) {
-        console.log("Error received from worker");
-      };
-      myWorker.postMessage("Hello from the main thread");
     }
   },
 };
